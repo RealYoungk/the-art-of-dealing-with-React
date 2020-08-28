51f0fea249440e991fcbdba7de3da574a140426e
realyoungk <9725394@naver.com>
2020-08-28 15:27:36 +0900

    Class Props

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,23 +1,27 @@
-import React from "react";
 import PropTypes from "prop-types";
+import React, { Component } from "react";
 
-const MyComponent = ({ name, children, favorateNumber }) => {
-  return (
-    <div>
-      안녕하세요 제이름은 <b>{name}</b> 입니다<br></br>
-      Children 값은 {children} 입니다
-      <br />
-      제가 좋아하는 숫자는 {favorateNumber} 입니다.
-    </div>
-  );
-};
+class MyComponent extends Component {
+  static defaultProps = {
+    name: "기본이름",
+  };
 
-MyComponent.defaultProps = {
-  name: "기본이름",
-};
+  static propTypes = {
+    name: PropTypes.string,
+    favorateNumber: PropTypes.number.isRequired,
+  };
 
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favorateNumber: PropTypes.number.isRequired,
-};
+  render() {
+    const { name, children, favorateNumber } = this.props;
+    return (
+      <div>
+        안녕하세요 제이름은 <b>{name}</b> 입니다<br></br>
+        Children 값은 {children} 입니다
+        <br />
+        제가 좋아하는 숫자는 {favorateNumber} 입니다.
+      </div>
+    );
+  }
+}
+
 export default MyComponent;
