24c668fc376f5e6ea2fcc2971af79586702f9880
realyoungk <9725394@naver.com>
2020-08-28 19:53:51 +0900

    #5. ref: DOM naming

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 // import Counter from "./Counter";
 // import Say from "./Say";
-import EventPractice from "./EventPractice";
+// import EventPractice from "./EventPractice";
+// import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBotton()}>맨밑으로</button>
+      </div>
+    );
+  }
+}
 
 export default App;
